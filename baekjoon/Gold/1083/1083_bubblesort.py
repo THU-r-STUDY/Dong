28bae8493cbd61d82206a01dfafc6f357b6ade63
Fidesss <miso4206@gmail.com>
2024-09-26 02:40:25 +0900

    Baekjoon no.1083

--- baekjoon/Gold/1083/1083_bubblesort.py
+++ baekjoon/Gold/1083/1083_bubblesort.py
@@ -1,13 +1,15 @@
-n = int(input())
-val = list(map(int, input().split()))
-s = int(input())
+n = int(input())                        # 입력 1
+val = list(map(int, input().split()))   # 입력 2
+s = int(input())                        # 입력 3
 
-for i in range(n):
-    max_val = max(val[i : i+1+s])
-    max_index = val.index(max_val)
-    while max_index != i and s:
-        val[max_index], val[max_index - 1] = val[max_index - 1], val[max_index]
-        max_index -= 1
+for i in range(n):                      # 반복문 1
+    max_val = max(val[i:i+s+1])         # 주어진 범위에서 최댓값 찾기
+    max_idx = val.index(max_val)        # 최댓값의 인덱스 추출
+    if s == 0:                          # s가 0이면 반복문 끝내기
+        break
+    while max_idx != i and s != 0:      # 반복문 2
+        val[max_idx], val[max_idx-1] = val[max_idx-1], val[max_idx]
         s -= 1
+        max_idx -= 1
 
-print(*val)
+print(*val)                             # unpacking후 출력
